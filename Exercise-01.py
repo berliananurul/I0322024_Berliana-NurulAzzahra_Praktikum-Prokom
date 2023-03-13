349302a2e1b250537cf63bea22390868c2a7e740
berliananurul <berliananurulazzahra@gmail.com>
2023-03-13 11:35:18 +0700

    Tugas Prokom 1

---
+++ Exercise-01.py
@@ -0,0 +1,4 @@
+("print : Berliana Nurul Azzahra")
+("print : I0322024")
+("print :       Saya tinggal di Perumahan Wirasana Indah, Purbalingga, Jawa Tengah. Lingkungan rumah tinggal saya masih termasuk asri karena masih banyak persawahan. Daerah tempat saya tinggal juga dekat dengan Gunung Slamet. Tetapi karena desa saya masih termasuk desa kecil jadi tidak terlalu banyak tempat untuk bermain, namun banyak juga tempat wisata yang masih asri.")
+("print :       Harapan saya di TI adalah saya dapat membawa nama baik TI UNS, dan saya berharap untuk mendapatkan pekerjaan yang sesuai dengan keinginan saya.")
