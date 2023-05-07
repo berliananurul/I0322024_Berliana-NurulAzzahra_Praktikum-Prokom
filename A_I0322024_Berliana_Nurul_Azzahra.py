429b3ba9d9635bf1d09ac819d894dabc184da9aa
berliananurul <berliananurulazzahra@gmail.com>
2023-05-07 20:45:25 +0700

    Exercise-06

---
+++ A_I0322024_Berliana_Nurul_Azzahra.py
@@ -0,0 +1,21 @@
+# masukkan tipe dan harga rumah
+tipe_45 = 550000000
+tipe_55 = 850000000
+tipe_60 = 950000000
+
+harga_rumah = int(input("masukkan tipe rumah= "))
+uang_muka = int(input("masukkan uang muka= "))
+tahun = int(input("masukkan jangka waktu tahun= "))
+suku_bunga = int(input("masukkan suku bunga per bulan= "))
+
+hutang = int(input("masukkan besar hutang= "))
+cicilan_pokokbulanan = int(input("masukkan cicilan pokok bulanan= "))
+cicilan_bungabulanan = int(input("masukkan cicilan bunga bulanan= "))
+total_cicilanbulanan = int(input("masukkan total cicilan bulanan= "))
+
+biaya_notaris = int(input("masukkan biaya notaris= "))
+biaya_provisi = int(input("masukkan biaya provisi= "))
+pajak_pembelian = int(input("masukkan pajak pembelian= "))
+PNBP = int(input("masukkan pnbp= "))
+biaya_baliknama = int(input("masukkan biaya balik nama= "))
+total_pembayaranpertama = int(input("masukkan total pembayaran pertama= "))
